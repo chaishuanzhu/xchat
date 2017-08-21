f3161ac5147f578ff4baf2c0b2160d24fa0a5eda
Colombo <870027381@qq.com>
2017-08-21 22:34:03 +0800

    first
    test

---
+++ server.php
@@ -0,0 +1,7 @@
+#!/usr/bin/env php
+<?php
+define('APP_PATH', __DIR__ . '/application/');
+define('BIND_MODULE','push/Worker');
+require '/data/www/xchat/vendor/autoload.php';
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
